df45515b1ef27e735cd8fe39976ff38fc73d7722
cristianQuev <119689796+cristianQuev@users.noreply.github.com>
2025-04-14 11:56:40 -0500

    fix css

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "../i18n/routing";
