c91dfaf9c7cfa39ddc3820b2f8bf6edd06da6d8b
cristianQuev <119689796+cristianQuev@users.noreply.github.com>
2025-04-14 11:54:44 -0500

    test initial

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import {useTranslations} from 'next-intl';
-import { Link } from './i18n/navigation';
+import { Link } from '../i18n/navigation';
  
 export default function HomePage() {
   const t = useTranslations('HomePage');
